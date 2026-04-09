59ed901ff4b36d5e1709928f1feea6f01cfc8b55
Kimoosabe <juliocesar-32@live.com>
2026-04-09 12:55:41 -0300

    Corrige nulabilidade da disponibilidade dos médicos

--- src/app/(protected)/doctors/_helpers/availability.ts
+++ src/app/(protected)/doctors/_helpers/availability.ts
@@ -8,20 +8,38 @@ import { doctorsTable } from "@/db/schema";
 dayjs.extend(utc);
 dayjs.locale("pt-br");
 
+const parseTime = (value: string | null | undefined) => {
+  const [hour = "8", minute = "0", second = "0"] = (value ?? "08:00:00").split(":");
+
+  return {
+    hour: Number(hour),
+    minute: Number(minute),
+    second: Number(second || 0),
+  };
+};
+
 export const getAvailability = (doctor: typeof doctorsTable.$inferSelect) => {
+  const firstRange = doctor.availabilityRanges?.[0];
+  const fromWeekDay = doctor.availableFromWeekDay ?? dayjs().day();
+  const toWeekDay = doctor.availableToWeekDay ?? fromWeekDay;
+  const fromTime = parseTime(firstRange?.fromTime ?? doctor.availableFromTime);
+  const toTime = parseTime(firstRange?.toTime ?? doctor.availableToTime);
+
   const from = dayjs()
     .utc()
-    .day(doctor.availableFromWeekDay)
-    .set("hour", Number(doctor.availableFromTime.split(":")[0]))
-    .set("minute", Number(doctor.availableFromTime.split(":")[1]))
-    .set("second", Number(doctor.availableFromTime.split(":")[2] || 0))
+    .day(fromWeekDay)
+    .set("hour", fromTime.hour)
+    .set("minute", fromTime.minute)
+    .set("second", fromTime.second)
     .local();
+
   const to = dayjs()
     .utc()
-    .day(doctor.availableToWeekDay)
-    .set("hour", Number(doctor.availableToTime.split(":")[0]))
-    .set("minute", Number(doctor.availableToTime.split(":")[1]))
-    .set("second", Number(doctor.availableToTime.split(":")[2] || 0))
+    .day(toWeekDay)
+    .set("hour", toTime.hour)
+    .set("minute", toTime.minute)
+    .set("second", toTime.second)
     .local();
+
   return { from, to };
 };
